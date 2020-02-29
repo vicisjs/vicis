3790a06b597e33b8e346dfa3495361bc716eefae
Anton Trofimenko <r37r0m0d3l@users.noreply.github.com>
2020-02-29 20:33:21 +0200

    Fix documentation text erros

--- test/index.test.js
+++ test/index.test.js
@@ -4,6 +4,6 @@ describe("calling and printing", () => {
   it("should not crash on call", () => {
     const log = jest.spyOn(console, "log").mockImplementation((any) => any);
     const vicis = new Vicis({});
-    expect(vicis.getConfig().sort).toBe(true);
+    expect(vicis.getConfig().sort).toBe(false);
   });
 });
