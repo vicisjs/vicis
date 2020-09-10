d9238adf779967df09100b10a31b59f623a04940
Anton Trofimenko <r37r0m0d3l@protonmail.com>
2020-09-11 00:13:17 +0300

    feat: data is not cloned between transformations

--- src/core/transform/transformData.ts
+++ src/core/transform/transformData.ts
@@ -1,7 +1,7 @@
 import { IObject } from "../../interface/common/IObject";
 import { ITransform } from "../../interface/config/ITransform";
 
-import { clone } from "../../util/variable/clone";
+// import { clone } from "../../util/variable/clone";
 import { isFunction } from "../../util/is/function";
 import { objectIsEmpty } from "../../util/object/is/empty";
 
@@ -26,14 +26,16 @@ export function transformData(
       dataToSerialize[key] = propertyValueTransformWith[key](
         dataToSerialize[key],
         key,
-        clone(dataToSerialize),
+        // clone(dataToSerialize),
+        dataToSerialize,
       );
     } else {
       // @ts-ignore
       dataToSerialize[key] = propertyValueTransformWith[key].toFunction()(
         dataToSerialize[key],
         key,
-        clone(dataToSerialize),
+        // clone(dataToSerialize),
+        dataToSerialize,
       );
     }
   });
